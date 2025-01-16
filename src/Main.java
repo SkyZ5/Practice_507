b6e069a2b15610107485eeabd4859c306a24ad67
BT_2S14_21 <kmei0013@yahoo.com>
2025-01-16 10:18:14 -0500

    Sample code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // step 1: create a file object
+        File f = new File("src/input_file");
+
+        String fileData = "";
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                fileData += currentLine + "\n";
+            }
+
+            // a String array where every item in the array is a line from the file
+            String[] fileArray = fileData.split("\n");
+
+            for (String line : fileArray) {
+                // split by space, now we have a list of String numbers
+                String[] stringNumbers = line.split(" ");
+
+                // create an array of integers
+                int[] numbers = new int[stringNumbers.length];
+
+                // convert string numbers into integers
+                for (int i = 0; i < numbers.length; i++) {
+                    numbers[i] = Integer.parseInt(stringNumbers[i]);
+                }
+            }
         }
+        catch (FileNotFoundException fe) {
+            System.out.println("File was not found");
+            System.exit(1);
+        }
+
     }
 }
